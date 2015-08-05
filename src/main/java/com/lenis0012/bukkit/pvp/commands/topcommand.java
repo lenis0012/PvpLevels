74708326562e29a30bdfc2bd6d29b925c81fa571
xTimon97 <zauber50@googlemail.com>
2015-08-05 22:44:12 +0200

    Create topcommand.java
    Get Toplevel from Players please help me

---
+++ src/main/java/com/lenis0012/bukkit/pvp/commands/topcommand.java
@@ -0,0 +1,19 @@
+	package com.lenis0012.bukkit.pvp.commands;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import com.lenis0012.bukkit.pvp.PvpLevels;
+import com.lenis0012.bukkit.pvp.PvpPlayer;
+
+public class LevelCommand implements CommandExecutor {
+	private PvpLevels plugin;
+	
+	public LevelCommand(PvpLevels plugin) {
+		this.plugin = plugin;
+	}
+int level = pp.getLevel()
+Colections.sort (level)
